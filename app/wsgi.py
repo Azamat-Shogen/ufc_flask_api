1757f2e77a67567d50d1f199100638ab87cd5fb2
Azamat Shogen <baki47hanma@gmail.com>
2021-12-17 20:30:55 -0600

    docker image set up

--- app/wsgi.py
+++ app/wsgi.py
@@ -3,5 +3,9 @@ from src import create_app
 
 app = create_app()
 
+@app.route('/')
+def hello():
+    return "welcome to the flask tutorials"
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
